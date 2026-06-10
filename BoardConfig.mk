a7a3f72dba8c4fd9071d6f02a5a3d7af9433ccb7
IRedDragonICY <hendik.suwoto@gmail.com>
2026-06-10 21:18:38 +0700

    fix(usb): enable USB OTG detection and controller host mode
    - init.recovery.usb.rc: Commented out the forced 'peripheral' mode override, allowing the DWC3 controller to switch to Host mode for OTG.
    
    - twrp.flags: Mapped /usb_otg to the correct block device (/dev/block/sdg) to properly mount external drives.
    
    - BoardConfig.mk: Added essential USB PHY and controller kernel modules (dwc3-msm.ko, phy-msm-ssusb-qmp.ko, xhci-sideband.ko, etc.) into TW_LOAD_VENDOR_MODULES to ensure the kernel enumerates OTG devices.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,5 @@
 #
+#
 # Copyright (C) 2023 The Android Open Source Project
 #
 # SPDX-License-Identifier: Apache-2.0
@@ -16,9 +17,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-# Building with minimal manifest
+BUILD_BROKEN_PLUGIN_VALIDATION := soong-libaosprecovery_defaults soong-libguitwrp_defaults soong-libminuitwrp_defaults soong-vold_defaults
+
+DEVICE_PATH := device/xiaomi/onyx
+
 ALLOW_MISSING_DEPENDENCIES := true
 
 # Architecture
@@ -180,7 +183,9 @@ else
 endif
 
 TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID := true
-TW_LOAD_VENDOR_MODULES := "adsp_loader_dlkm.ko nt38771_touch.ko nxp-nci.ko xiaomi_touch.ko"
+TW_LOAD_VENDOR_MODULES := "adsp_loader_dlkm.ko nt38771_touch.ko nxp-nci.ko xiaomi_touch.ko phy-msm-m31-eusb2.ko phy-msm-snps-eusb2.ko phy-msm-ssusb-qmp.ko repeater-qti-pmic-eusb2.ko wcd_usbss_i2c.ko dwc3-msm.ko xhci-sideband.ko repeater.ko phy-generic.ko"
 TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI := true
 TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone48/temp"
 TW_BATTERY_SYSFS_WAIT_SECONDS := 6
+TW_VIRTUAL_AB_COMPRESSION := true
+TW_INCLUDE_LIBRESETPROP := true
