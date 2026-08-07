cd351af03bfc064c5b573de0b0673aa21fc362b3
Jindong Yang <yjindong@outlook.com>
2026-08-07 13:36:06 +0800

    fix: remove TW_CUSTOM_CPU_TEMP_PATH override that pointed to static /tmp/cpu_temp (always 0)

--- fox_onyx.mk
+++ fox_onyx.mk
@@ -96,4 +96,5 @@ OF_ENABLE_FRP_ADDON := 1
 
 # Set to 1 to force the casefolding props to true. Useful for devices that shipped with Android 11+/FBEv2, where casefolding is always used
 OF_FORCE_CASEFOLDING := 1
-TW_CUSTOM_CPU_TEMP_PATH := /tmp/cpu_temp
+# Removed: TW_CUSTOM_CPU_TEMP_PATH override was pointing to static /tmp/cpu_temp = always 0
+# Now uses BoardConfig.mk value: /sys/class/thermal/thermal_zone48/temp
